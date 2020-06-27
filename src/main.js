fb301174a97b01a47f99ac1c1c7e9489d22f8d33
taoey <hwt8080@163.com>
2020-06-27 09:14:20 +0800

    自定义init（引入iview）

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+
+import ViewUI from 'view-design';
+import 'view-design/dist/styles/iview.css';
+
+Vue.use(ViewUI);
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
